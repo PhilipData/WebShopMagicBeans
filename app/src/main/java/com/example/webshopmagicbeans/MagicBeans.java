d0c962c3307a3eee993bf7304cb4efce757b3f5a
PhilipData <philippeterssonit@gmail.com>
2024-09-06 09:45:56 +0200

    created the MagicBeans class

---
+++ app/src/main/java/com/example/webshopmagicbeans/MagicBeans.java
@@ -0,0 +1,16 @@
+package com.example.webshopmagicbeans;
+
+public class MagicBeans {
+    int price;
+    int rarity;
+    String name;
+    String description;
+    String sideEffects;
+    Colors color;
+
+
+}
+
+enum Colors {
+    BLACK,RED,VIOLET,BLUE,PINK,YELLOW,POLKADOTTED,ZEBRASTRIPED,RAINBOW,GREEN,BONEWHITE
+        }
